991168e1bb48c5eb7f1214e64eae56bf585933be
matt <matthieu.l.dubray@gmail.com>
2023-02-22 15:52:33 -0800

    import bootstrap/bootstrap icons created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserLoginSignupComponent } from './user-login-signup/user-login-signup.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { AnswerFormComponent } from './answer-form/answer-form.component';
+import { QuestionPageComponent } from './question-page/question-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserLoginSignupComponent,
+    LandingPageComponent,
+    QuestionFormComponent,
+    AnswerFormComponent,
+    QuestionPageComponent
   ],
   imports: [
     BrowserModule
